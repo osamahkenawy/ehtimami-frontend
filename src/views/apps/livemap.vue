ebd7063e3ecaa6572936bffe0aced02a68977e1b
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2024-10-13 22:49:54 +0400

    [8-create-reusable-component-for-vehicles-live-map] FinalizeBreadCrumb

--- src/views/apps/livemap.vue
+++ src/views/apps/livemap.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-      <!-- LocationMap Component -->
-      <UAEPlate plateNumber="12-DUB-3456" plateColor="red" :flat="false" />
-      <div class="mt-5">
-        <LocationMap />
-      </div>
+  <div>
+    <!-- Breadcrumb for the Livemap Page -->
+    <BreadCrumb :items="breadcrumbItems" />
+
+    <!-- UAEPlate Component -->
+    <UAEPlate plateNumber="12-DUB-3456" plateColor="red" :flat="false" />
+
+    <!-- LocationMap Component -->
+    <div class="mt-5">
+      <LocationMap />
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-    // Import the LocationMap component
-    import LocationMap from '@/components/maps/LocationMap.vue';
-    import UAEPlate from '@/components/plates/UAEPlate.vue';
-  </script>
-  
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import BreadCrumb from '@/components/BreadCrumb.vue';
+  import LocationMap from '@/components/maps/LocationMap.vue';
+  import UAEPlate from '@/components/plates/UAEPlate.vue';
+  import IconHome from '@/components/icon/icon-home.vue';
+
+  // Define the breadcrumb items for this page
+  const breadcrumbItems = [
+    { label: 'Home', link: '/', icon: IconHome },
+    { label: 'Components', link: '/apps/todolist' },
+    { label: 'UI Kit', link: '/apps/mailbox' },
+    { label: 'Livemap' }
+  ];
+</script>
