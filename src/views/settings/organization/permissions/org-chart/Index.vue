796bee09b97456457a9692fc60a4a53455d5f7c3
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2025-02-16 10:35:49 +0400

    [45-organizations] intialize The main org chart components

--- src/views/settings/organization/permissions/org-chart/Index.vue
+++ src/views/settings/organization/permissions/org-chart/Index.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-            OrganizationChart
-    </div>
+   <div>
+      <div class="flex justify-between items-center mb-4">
+        <BreadCrumb :items="breadcrumbItems" />
+      </div>
+      
+    </div> 
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+import IconHome from "@/components/icon/icon-home.vue";
+import iconOrgChart from "@/components/icon/icon-org-chart.vue";
 
+  const { t } = useI18n();
+  const breadcrumbItems = computed(() => [
+    { label: "Home", link: "/", icon: IconHome },  
+    { label: t("organization"), link: "/", icon: iconOrgChart },
+    { label: t("organization-chart") },
+  ]);
 </script>
 
-<style scoped>
+<style scoped> 
 
 </style>
