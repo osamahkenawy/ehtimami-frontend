28a84168fe161d8cdd6267f770b999428d09706c
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2025-02-19 10:37:04 +0400

    [45-organizations] add New SidebarsIcons

---
+++ src/components/icon/sidebarIcons/icon-asset.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+  <path d="M33 39.2998V41.9998" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M15 39.2998V41.9998" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M9.6001 14.6073C9.6001 13.4565 9.6001 12.8811 9.91306 12.1804C10.226 11.4797 10.5438 11.1949 11.1793 10.6254C13.262 8.75899 17.4486 6 24.0001 6C30.5516 6 34.7382 8.75899 36.8208 10.6254C37.4564 11.1949 37.7742 11.4797 38.0871 12.1804C38.4001 12.8811 38.4001 13.4565 38.4001 14.6073V27.6C38.4001 32.6911 38.4001 35.2367 36.8184 36.8183C35.2368 38.4 32.6912 38.4 27.6001 38.4H20.4001C15.3089 38.4 12.7633 38.4 11.1817 36.8183C9.6001 35.2367 9.6001 32.6911 9.6001 27.6V14.6073Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"/>
+  <path d="M9.6001 27.6001C9.6001 27.6001 16.3201 29.4001 24.0001 29.4001C31.6802 29.4001 38.4001 27.6001 38.4001 27.6001" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M10.5 33.8999H13.2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M34.8 33.8999H37.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M22.2 33.8999H25.8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M9.6001 13.2002H38.4001" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"/>
+  <path d="M6 18.6001V20.4001" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M42 18.6001V20.4001" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</template>
---
+++ src/components/icon/sidebarIcons/icon-event-road.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+  <path d="M42.0001 8H6M42.0001 40H6" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M14.2696 19.322C18.0899 21.3418 20 22.3518 20 24C20 25.6482 18.0899 26.6582 14.2696 28.678L11.668 30.0534C8.73411 31.6046 7.26713 32.3802 6.56181 31.8164C6.37219 31.6648 6.21575 31.465 6.10471 31.2326C5.69175 30.3682 6.52371 28.7816 8.18765 25.6086C8.55673 24.9048 8.74125 24.5528 8.77427 24.168C8.78385 24.0562 8.78385 23.9438 8.77427 23.832C8.74127 23.4472 8.55673 23.0952 8.18765 22.3914C6.52371 19.2183 5.69175 17.6317 6.10471 16.7674C6.21575 16.5351 6.37219 16.3353 6.56181 16.1837C7.26713 15.6198 8.73411 16.3954 11.668 17.9466L14.2696 19.322Z" stroke="currentColor" stroke-width="3"/>
+  <path d="M42 24H38" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M30 24H26" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</template>
