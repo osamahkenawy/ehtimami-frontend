60ff9076358b06cb252889dc682143162e58da52
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2025-04-13 09:46:04 +0400

    Add Icon Eye Off For signin

--- src/views/auth/boxed-signin.vue
+++ src/views/auth/boxed-signin.vue
@@ -1,143 +1,221 @@
 <template>
-    <div>
-        <div class="absolute inset-0">
-            <img src="/assets/images/auth/heroo-bg.jpg" alt="image" class="h-full w-full object-cover" />
-        </div>
-        <div
-            class="relative flex min-h-screen items-center justify-center  bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
-           
-            <div
-                class="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]"
-            >
-                <div class="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[758px] py-20">
-                    <div class="absolute top-6 end-6">
-                        <div class="dropdown">
-                            <Popper :placement="store.rtlClass === 'rtl' ? 'bottom-start' : 'bottom-end'" offsetDistance="8">
-                                <button
-                                    type="button"
-                                    class="flex items-center gap-2.5 rounded-lg border border-white-dark/30 bg-white px-2 py-1.5 text-white-dark hover:border-primary hover:text-primary dark:bg-black"
-                                >
-                                    <div>
-                                        <img :src="currentFlag" alt="image" class="h-5 w-5 rounded-full object-cover" />
-                                    </div>
-                                    <div class="text-base font-bold uppercase">{{ store.locale }}</div>
-                                    <span class="shrink-0">
-                                        <icon-caret-down />
-                                    </span>
-                                </button>
-                                <template #content="{ close }">
-                                    <ul class="!px-2 text-dark dark:text-white-dark grid grid-cols-2 gap-2 font-semibold dark:text-white-light/90 w-[280px]">
-                                        <template v-for="item in store.languageList" :key="item.code">
-                                            <li>
-                                                <button
-                                                    type="button"
-                                                    class="w-full hover:text-primary"
-                                                    :class="{ 'bg-primary/10 text-primary': i18n.locale === item.code }"
-                                                    @click="changeLanguage(item), close()"
-                                                >
-                                                    <img
-                                                        class="w-5 h-5 object-cover rounded-full"
-                                                        :src="`/assets/images/flags/${item.code.toUpperCase()}.svg`"
-                                                        alt=""
-                                                    />
-                                                    <span class="ltr:ml-3 rtl:mr-3">{{ item.name }}</span>
-                                                </button>
-                                            </li>
-                                        </template>
-                                    </ul>
-                                </template>
-                            </Popper>
-                        </div>
-                    </div>
-                    <div class="mx-auto w-full max-w-[440px]">
-                        <div class="mb-10">
-                            <!-- add Logo Here  -->
-                            <img src="/assets/images/ehtimamiLogo.svg" alt="image" class="h-full w-full object-cover" />
-                            <h1 class="text-2xl font-extrabold uppercase !leading-snug text-primary md:text-4xl mt-2">{{ $t('sign_in') }}</h1>
-                            <p class="text-base font-bold leading-normal text-white-dark">{{ $t('enter_email_password') }}</p>
-                        </div>
-                        <form class="space-y-5 dark:text-white" @submit.prevent="router.push('/')">
-                            <div>
-                                <label for="Email">{{ $t('email') }}</label>
-                                <div class="relative text-white-dark">
-                                    <input v-model="email" id="Email" type="email" placeholder="Enter Email" class="form-input ps-10 placeholder:text-white-dark" />
-                                    <span class="absolute start-4 top-1/2 -translate-y-1/2">
-                                        <icon-mail :fill="true" />
-                                    </span>
-                                </div> 
-                            </div>
-                            <div>
-                                <label for="Password">{{ $t('password') }}</label>
-                                <div class="relative text-white-dark">
-                                    <input v-model="password" id="Password" type="password" placeholder="Enter Password" class="form-input ps-10 placeholder:text-white-dark" />
-                                    <span class="absolute start-4 top-1/2 -translate-y-1/2">
-                                        <icon-lock-dots :fill="true" />
-                                    </span>
-                                </div>
-                                <div class="mt-2 text-end">
-                                    <router-link to="/auth/forgot-password" class="text-sm font-semibold text-primary hover:underline">
-                                        {{ $t('forgot_password') }}
-                                    </router-link>
-                                </div>
-                            </div>
-                         
-                            <button type="button" @click="login" class="btn btn-dark !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                                {{ $t('sign_in') }}
-                            </button> 
-                        </form>
-                        <div class="relative my-7 text-center md:mb-9">
-                            <span class="absolute inset-x-0 top-1/2 h-px w-full -translate-y-1/2 bg-white-light dark:bg-white-dark"></span>
-                            <span class="relative bg-white px-2 font-bold uppercase text-white-dark dark:bg-dark dark:text-white-light">{{ $t('or') }}</span>
-                        </div>
-                        <div class="text-center dark:text-white">
-                            {{ $t('no_account') }}
-                            <router-link to="/auth/boxed-signup" class="uppercase text-primary underline transition hover:text-black dark:hover:text-white">
-                                {{ $t('sign_up') }}
-                            </router-link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div>
+    <div class="absolute inset-0">
+      <img
+        src="/assets/images/auth/heroo-bg.jpg"
+        alt="image"
+        class="h-full w-full object-cover"
+      />
     </div>
+    <div
+      class="relative flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16"
+    >
+      <div
+        class="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]"
+      >
+        <div
+          class="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[758px] py-20"
+        >
+          <div class="absolute top-6 end-6">
+            <div class="dropdown">
+              <Popper
+                :placement="
+                  store.rtlClass === 'rtl' ? 'bottom-start' : 'bottom-end'
+                "
+                offsetDistance="8"
+              >
+                <button
+                  type="button"
+                  class="flex items-center gap-2.5 rounded-lg border border-white-dark/30 bg-white px-2 py-1.5 text-white-dark hover:border-primary hover:text-primary dark:bg-black"
+                >
+                  <div>
+                    <img
+                      :src="currentFlag"
+                      alt="image"
+                      class="h-5 w-5 rounded-full object-cover"
+                    />
+                  </div>
+                  <div class="text-base font-bold uppercase">
+                    {{ store.locale }}
+                  </div>
+                  <span class="shrink-0">
+                    <icon-caret-down />
+                  </span>
+                </button>
+                <template #content="{ close }">
+                  <ul
+                    class="!px-2 text-dark dark:text-white-dark grid grid-cols-2 gap-2 font-semibold dark:text-white-light/90 w-[280px]"
+                  >
+                    <template
+                      v-for="item in store.languageList"
+                      :key="item.code"
+                    >
+                      <li>
+                        <button
+                          type="button"
+                          class="w-full hover:text-primary"
+                          :class="{
+                            'bg-primary/10 text-primary':
+                              i18n.locale === item.code,
+                          }"
+                          @click="changeLanguage(item), close()"
+                        >
+                          <img
+                            class="w-5 h-5 object-cover rounded-full"
+                            :src="`/assets/images/flags/${item.code.toUpperCase()}.svg`"
+                            alt=""
+                          />
+                          <span class="ltr:ml-3 rtl:mr-3">{{ item.name }}</span>
+                        </button>
+                      </li>
+                    </template>
+                  </ul>
+                </template>
+              </Popper>
+            </div>
+          </div>
+          <div class="mx-auto w-full max-w-[440px]">
+            <div class="mb-10">
+              <!-- add Logo Here  -->
+              <img
+                src="/assets/images/ehtimamiLogo.svg"
+                alt="image"
+                class="h-full w-full object-cover"
+              />
+              <h1
+                class="text-2xl font-extrabold uppercase !leading-snug text-primary md:text-4xl mt-2"
+              >
+                {{ $t("sign_in") }}
+              </h1>
+              <p class="text-base font-bold leading-normal text-white-dark">
+                {{ $t("enter_email_password") }}
+              </p>
+            </div>
+            <form
+              class="space-y-5 dark:text-white"
+              @submit.prevent="router.push('/')"
+            >
+              <div>
+                <label for="Email">{{ $t("email") }}</label>
+                <div class="relative text-white-dark">
+                  <input
+                    v-model="email"
+                    id="Email"
+                    type="email"
+                    placeholder="Enter Email"
+                    class="form-input ps-10 placeholder:text-white-dark"
+                  />
+                  <span class="absolute start-4 top-1/2 -translate-y-1/2">
+                    <icon-mail :fill="true" />
+                  </span>
+                </div>
+              </div>
+              <div>
+                <label for="Password">{{ $t("password") }}</label>
+                <div class="relative text-white-dark">
+                  <input
+                    v-model="password"
+                    :type="showPassword ? 'text' : 'password'"
+                    placeholder="Enter Password"
+                    class="form-input ps-10 pe-10 placeholder:text-white-dark"
+                  />
+                  <span class="absolute start-4 top-1/2 -translate-y-1/2">
+                    <icon-lock-dots :fill="true" />
+                  </span>
+                  <span
+                    class="absolute end-4 top-1/2 -translate-y-1/2 cursor-pointer"
+                    @click="showPassword = !showPassword"
+                  >
+                    <component :is="passwordToggleIcon" />
+                  </span>
+                </div>
+                <div class="mt-2 text-end">
+                  <router-link
+                    to="/auth/forgot-password"
+                    class="text-sm font-semibold text-primary hover:underline"
+                  >
+                    {{ $t("forgot_password") }}
+                  </router-link>
+                </div>
+              </div>
+
+              <button
+                type="button"
+                @click="login"
+                class="btn btn-dark !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
+              >
+                {{ $t("sign_in") }}
+              </button>
+            </form>
+            <div class="relative my-7 text-center md:mb-9">
+              <span
+                class="absolute inset-x-0 top-1/2 h-px w-full -translate-y-1/2 bg-white-light dark:bg-white-dark"
+              ></span>
+              <span
+                class="relative bg-white px-2 font-bold uppercase text-white-dark dark:bg-dark dark:text-white-light"
+                >{{ $t("or") }}</span
+              >
+            </div>
+            <div class="text-center dark:text-white">
+              {{ $t("no_account") }}
+              <router-link
+                to="/auth/boxed-signup"
+                class="uppercase text-primary underline transition hover:text-black dark:hover:text-white"
+              >
+                {{ $t("sign_up") }}
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-    import { computed, reactive } from 'vue';
-    import { useI18n } from 'vue-i18n';
-    import appSetting from '@/app-setting';
-    import { useAppStore } from '@/stores/index';
-    import { useRouter } from 'vue-router';
-    import { useMeta } from '@/composables/use-meta';
-    import { ref } from 'vue';
-    import IconCaretDown from '@/components/icon/icon-caret-down.vue';
-    import IconMail from '@/components/icon/icon-mail.vue';
-    import IconLockDots from '@/components/icon/icon-lock-dots.vue';
-    import { useAuthStore } from '@/stores/auth';
-    
-    useMeta({ title: 'Login Boxed' });
-    const email = ref<string>('');
-    const password = ref<string>('');
-    const router = useRouter();
-    const authStore = useAuthStore();
-    const store = useAppStore();
-    // multi language
-    const i18n = reactive(useI18n());
-    const { t } = useI18n();
-    const changeLanguage = (item: any) => {
-        i18n.locale = item.code;
-        appSetting.toggleLanguage(item);
-    };
-    const currentFlag = computed(() => {
-        return `/assets/images/flags/${i18n.locale.toUpperCase()}.svg`;
-    });
-    const login = async (): Promise<void> => {
-    try {
-        const success = await authStore.login(email.value, password.value);
-        if (success) {
-            router.push('/');
-        }
-    } catch (error) {
-        console.error("Login failed:", error);
+import { computed, reactive } from "vue";
+import { useI18n } from "vue-i18n";
+import appSetting from "@/app-setting";
+import { useAppStore } from "@/stores/index";
+import { useRouter } from "vue-router";
+import { useMeta } from "@/composables/use-meta";
+import { ref } from "vue";
+import IconCaretDown from "@/components/icon/icon-caret-down.vue";
+import IconMail from "@/components/icon/icon-mail.vue";
+import IconLockDots from "@/components/icon/icon-lock-dots.vue";
+import { useAuthStore } from "@/stores/auth";
+import IconEye from "@/components/icon/icon-eye.vue";
+import IconEyeOff from "@/components/icon/icon-eye-off.vue";
+
+const passwordToggleIcon = computed(() =>
+  showPassword.value ? IconEyeOff : IconEye
+);
+
+const showPassword = ref(false);
+
+useMeta({ title: "Login Boxed" });
+const email = ref<string>("");
+const password = ref<string>("");
+const router = useRouter();
+const authStore = useAuthStore();
+const store = useAppStore();
+// multi language
+const i18n = reactive(useI18n());
+const { t } = useI18n();
+const changeLanguage = (item: any) => {
+  i18n.locale = item.code;
+  appSetting.toggleLanguage(item);
+};
+const currentFlag = computed(() => {
+  return `/assets/images/flags/${i18n.locale.toUpperCase()}.svg`;
+});
+const login = async (): Promise<void> => {
+  try {
+    const success = await authStore.login(email.value, password.value);
+    if (success) {
+      router.push("/");
     }
+  } catch (error) {
+    console.error("Login failed:", error);
+  }
 };
 </script>
